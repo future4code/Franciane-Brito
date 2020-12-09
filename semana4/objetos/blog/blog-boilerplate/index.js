1dee425e85d69134e6706104c82b83be5fed2b93
franciane-brito <francianebrito99@gmail.com>
2020-12-09 16:43:36 -0300

    Ultimas alterações no projeto básico

--- semana4/objetos/blog/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -1,29 +1,39 @@
+let campoTitulo = document.getElementById("titulo-post")
+let campoAutor = document.getElementById("autor-post")
+let campoConteudo = document.getElementById("conteudo-post")
+
+let campoImagem = document.getElementById("imagem-post")
+
+
 let posts = document.getElementById("container-de-posts")
 
-const informacoesPost = {
-    titulo: document.getElementById("titulo-post"),
-    autor: document.getElementById("autor-post"),
-    conteudo: document.getElementById("conteudo-post"),
-    imagem: document.getElementById("imagem-post")
-}
 
+const informacoesPost = {
+    titulo: campoTitulo,
+    autor: campoAutor,
+    conteudo: campoConteudo,
+    imagem: campoImagem
+}
 
 function teste() {
 
-    posts.innerHTML += `<section><h3>${informacoesPost.titulo.value}</h3><h4>${informacoesPost.autor.value}</h4><p>${informacoesPost.conteudo.value}</p></section>`
-    if (informacoesPost.imagem.value.includes(http)) {
-        posts.innerHTML += `<img src=${campoImagem.value}/>`
-    }
+    posts.innerHTML += `<section><h3>${campoTitulo.value}</h3><h4>${campoAutor.value}</h4><p>${informacoesPost.conteudo.value}</p></section>`
     informacoesPost.titulo.value = ''
     informacoesPost.autor.value = ''
     informacoesPost.conteudo.value = ''
+        /* if (informaocoesPost.imagem.value.includes(http)) {
+
+            posts.innerHTML += `<img src=${campoImagem.value}/>`
+        } */
     informacoesPost.imagem.value = ''
+    console.log(informacoesPost)
 }
 
 
-/* function inserirImagem() {
+
+function inserirImagem() {
     console.log(campoImagem)
         /* if (campoImagem.includes(https)) {
             console.log("estouDentro")
-        } 
-} */
+        } */
+}
