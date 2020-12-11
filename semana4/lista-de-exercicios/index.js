b9cff9c85f7923f525166ecbae56720eb3e206b6
franciane-brito <francianebrito99@gmail.com>
2020-12-11 14:44:33 -0300

    Exercicios de interpretação de código 1 ao 3

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,15 @@
+/* Exercícios de interpretação de código */
+
+
+/* 1- A função converte o valor de dolares para outra moeda. Para isso ela recebe o valor 
+da cotação e o valor em dolar e multiplica. */
+
+/* 2- A função tem um loop que mostra quanto o usuario vai ter de lucros de acordo com 
+o tipo de investimento que ele fizer. novoMontante vai retornar 165 e segundoMontante vai retornar
+ "Tipo de investimento informado incorreto!" */
+
+/* 3- O codigo usa como base o array numeros para formar outros dois arrays um de numeros pares e
+outro de numeros impares. O Loop adiciona cada elemento ao seu respectivo array verificando se é par
+ou impar. No final vai aoarecer no console o tamanho do array numeros, array e array2 */
+
+
