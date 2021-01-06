8cc3e2a0a77db97a8d513aa88d6c467ac72d0d7c
franciane-brito <francianebrito99@gmail.com>
2021-01-06 00:53:14 -0300

    Resolvendo problema na function de selecionar tarefas

--- semana6/ciclo-de-vida/task-list/src/App.js
+++ semana6/ciclo-de-vida/task-list/src/App.js
@@ -9,7 +9,7 @@ const TarefaList = styled.ul`
 
 const Tarefa = styled.li`
   text-align: left;
-  text-decoration: ${({completaVerifica}) => (completaVerifica ? 'line-through' : 'none')};
+  text-decoration: ${({ completa }) => (completa ? 'line-through' : 'none')};
 `
 
 const InputsContainer = styled.div`
@@ -17,105 +17,222 @@ const InputsContainer = styled.div`
   grid-auto-flow: column;
   gap: 10px;
 `
-
 class App extends React.Component {
   state = {
     tarefas: [],
-   
+
     inputValue: '',
     filtro: ''
   }
 
-componentDidUpdate() {
+ /*  componentDidUpdate() {
+   
+   const tarefas = {
+      id: this.state.id,
+      texto: this.state.texto,
+      completa: this.state.completa
+    };
 
+    console.log("Tarefas", tarefas);
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  };
+
+  componentDidMount() {
+    const tarefasString = localStorage.getItem("tarefas")
+    const tarefasObjeto = JSON.parse(tarefasString)
+
+    if(tarefasObjeto) {
+      this.setState({
+        id: tarefasObjeto.id,
+        texto: tarefasObjeto.texto,
+        completa: tarefasObjeto.completa
+      })
+    }
+
+  };
+
+aoMudarNome = (e) => {
+  this.setState({
+    id: this.state.tarefas.id
+  });
 };
 
-componentDidMount() {
-
+aoMudarEmail = (e) => {
+  this.setState({
+    texto: e.target.value
+  });
 };
 
-onChangeInput = (event) => {
-  this.setState({inputValue: event.target.value}) 
-}
+aoMudarIdade = (e) => {
+  this.setState({
+    completa: e.target.value
+  });
+};
+ */
 
-criaTarefa = () => {
-  console.log("Vou criar uma tarefa", this.state.tarefas)
-  const novaTarefa = {
+  onChangeInput = (event) => {
+    this.setState({ inputValue: event.target.value })
+  }
+
+  criaTarefa = () => {
+    console.log("Vou criar uma tarefa", this.state.tarefas)
+    const novaTarefa = {
       id: Date.now(),
       texto: this.state.inputValue,
       completa: false
+    } 
+
+    const novaListaDeTarefas = [... this.state.tarefas, novaTarefa]
+
+    this.setState({ tarefas: novaListaDeTarefas })
+
+    this.state.inputValue = ''
   }
-  
-  const novaListaDeTarefas = [... this.state.tarefas, novaTarefa]
 
-this.setState({tarefas: novaListaDeTarefas})
-
-this.state.inputValue = ''
-
-}
-
-selectTarefa = (id) => {
-    const tarefasSelecionadas = this.state.tarefas.map((tarefa) =>{
-      if(id === tarefa.id){
+  selectTarefa = (id) => {
+    console.log('Entrei na função select tarefa')
+    const tarefasSelecionadas = this.state.tarefas.map((tarefa) => {
+      if (id === tarefa.id) {
+        console.log('Tarefa', tarefa)
         const novaTarefa = {
           ...tarefa,
           completa: !tarefa.completa
         }
+        console.log('Tarefa', novaTarefa)
         return novaTarefa
+      } else {
+        return tarefa
       }
     })
-    this.setState({tarefas: tarefasSelecionadas})
-}
+    this.setState({ tarefas: tarefasSelecionadas })
+  }
 
-onChangeFilter = (event) => {
+  onChangeFilter = (event) => {
+    
 
-}
-  
-render() {
-  const listaFiltrada = this.state.tarefas.filter(tarefa => {
-    switch (this.state.filtro) {
-      case 'pendentes':
-        return !tarefa.completa
-      case 'completas':
-        return tarefa.completa
-      default:
-        return true
-    }
-  })
+    this.setState({ filtro: event.target.value })
+  }
 
-  return (
-    <div className="App">
-      <h1>Lista de tarefas</h1>
-      <InputsContainer>
-        <input value={this.state.inputValue} onChange={this.onChangeInput}/>
-        <button onClick={this.criaTarefa}>Adicionar</button>
-      </InputsContainer>
-      <br/>
+  render() {
+    const listaFiltrada = this.state.tarefas.filter((tarefa) => {
+      switch (this.state.filtro) {
+        case 'pendentes':
+          return !tarefa.completa
+        case 'completas':
+          return tarefa.completa
+        default:
+          return true
+      }
+    })
 
-      <InputsContainer>
-        <label>Filtro</label>
-        <select value={this.state.filter} onChange={this.onChangeFilter}>
-          <option value="">Nenhum</option>
-          <option value="pendentes">Pendentes</option>
-          <option value="completas">Completas</option>
-        </select>
-      </InputsContainer>
-      <TarefaList >
-        {listaFiltrada.map((tarefa) => {
-          return (
-            <Tarefa
-            completaVerifica={tarefa.completa}
-              onClick={() => this.selectTarefa(tarefa.id)}
-            >
-              {tarefa.texto}
-            </Tarefa>
+    return (
+      <div className="App">
+        <h1>Lista de tarefas</h1>
+        <InputsContainer>
+          <input value={this.state.inputValue} onChange={this.onChangeInput} />
+          <button onClick={this.criaTarefa} >Adicionar</button>
+        </InputsContainer>
+        <br />
+
+        <InputsContainer>
+          <label>Filtro</label>
+          <select value={this.state.filter} onChange={this.onChangeFilter}>
+            <option value="">Nenhum</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="completas">Completas</option>
+          </select>
+        </InputsContainer>
+        <TarefaList >
+          {listaFiltrada.map((tarefa) => {
+            return (
+              <Tarefa
+                completa={tarefa.completa}
+                onClick={() => this.selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+
+              </Tarefa>
           )
         })}
-      </TarefaList>
-    </div>
-  )
-}
+        </TarefaList>
+        </div>
+    )
+  }
 }
 
-export default App 
+export default App
 
+
+/*  import React from "react";
+
+class Formulario extends React.Component {
+  state = {
+    nome: "",
+    email: "",
+    idade: ""
+  };
+
+  componentDidMount() {
+    const usuarioString = localStorage.getItem("usuario");
+    const usuarioObjeto = JSON.parse(usuarioString);
+    
+    if(usuarioObjeto) {
+      this.setState({
+        nome: usuarioObjeto.nome,
+        email: usuarioObjeto.email,
+        idade: usuarioObjeto.idade
+      })
+    }
+
+  }
+
+  //onChangeName
+  aoMudarNome = (e) => {
+    this.setState({
+      nome: e.target.value
+    });
+  };
+
+  //onChangeEmail
+  aoMudarEmail = (e) => {
+    this.setState({
+      email: e.target.value
+    });
+  };
+
+  //onChangeAge
+  aoMudarIdade = (e) => {
+    this.setState({
+      idade: e.target.value
+    });
+  };
+
+  componentDidUpdate() {
+    const usuario = {
+      nome: this.state.nome,
+      email: this.state.email,
+      idade: this.state.idade
+    };
+
+    console.log("USUÁRIO", usuario);
+
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Nome:</p>
+        <input value={this.state.nome} onChange={this.aoMudarNome} />
+        <p>Email: </p>
+        <input value={this.state.email} onChange={this.aoMudarEmail} />
+        <p>Idade: </p>
+        <input value={this.state.idade} onChange={this.aoMudarIdade} />
+      </div>
+    );
+  }
+}
+
+export default Formulario;
+*/
