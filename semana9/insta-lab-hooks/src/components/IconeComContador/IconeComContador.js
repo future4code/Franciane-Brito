a8498aeee4939126c10e6b347f6c98594b165bbb
franciane brito <francianebrito99@gmail.com>
2021-01-25 14:43:36 -0300

    Atualização

--- semana9/insta-lab-hooks/src/components/IconeComContador/IconeComContador.js
+++ semana9/insta-lab-hooks/src/components/IconeComContador/IconeComContador.js
@@ -19,24 +19,3 @@ function IconeComContador(props) {
 export default IconeComContador
 
 
-/* 
-import React from 'react'
-import styled from "styled-components"
-
-const IconeContainer = styled.div`
-	display: flex;
-
-	img {
-		margin-right: 5px;
-	}
-`
-
-function IconeComContador(props) {
-	return <IconeContainer>
-		<img alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
-		<p>{props.valorContador}</p>
-	</ IconeContainer>
-}
-
-export default IconeComContador
-*/
