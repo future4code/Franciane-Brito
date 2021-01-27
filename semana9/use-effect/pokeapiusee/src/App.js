24d9193ddfc26363dfc72c8603e0fe798d8454d0
franciane brito <francianebrito99@gmail.com>
2021-01-26 22:36:07 -0300

    Finalizando exercicios obrigatórios

--- semana9/use-effect/pokeapiusee/src/App.js
+++ semana9/use-effect/pokeapiusee/src/App.js
@@ -1,172 +1,49 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PokeCard from "./Components/PokeCard";
-import './App.css';
 
 function App() {
-  const [pokeName, setPokeName] = useState('')
   const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState('')
 
- 
 
-  const handlePokemon = () => {
+
+  const handlePokemons = () => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-      .then(response => {
+      .then((response) => {
         setPokeList(response.data.results)
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
-  useEffect(() => {
-    handlePokemon()
-  })
-
-
-  const changePokeName = (event) => {
-    setPokeName(event.target.value)
-  };
-
-
-    return (
-      <div className="App">
-       
-        <select onChange={changePokeName}>
-          <option value={""}>Nenhum</option>
-          {pokeList.map(pokemon => {
-            return (
-              <option key={pokemon.name} value={pokemon.name}>
-                {pokemon.name}
-              </option>
-            );
-          })}
-        </select>
-        {pokeName && <PokeCard pokemon={pokeName} />}
-      </div>
-    );
-}
-
-export default App;
-
-
-/* 
-
-class App extends React.Component {
-  state = {
-    // lista de pokemons que está sendo guardada no estado
-    pokeList: [],
-    // nome do pokemon guardado no estado, assim que o usuário
-    // escolhe um nome no dropdown
-    pokeName: ""
-  };
-
-  // método que roda após a montagem do componente
-  componentDidMount = () => {
-    // função axios que está batendo na API e buscando 151 pokemons
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
-      .then(response => {
-        // função que está setando no estado os 151 pokemons
-        this.setState({ pokeList: response.data.results });
-      })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      });
-  };
-
-  changePokeName = event => {
-    this.setState({ pokeName: event.target.value });
-  };
-
-  render() {
-    return (
-      <div className="App">
-       
-        <select onChange={this.changePokeName}>
-          <option value={""}>Nenhum</option>
-          {this.state.pokeList.map(pokemon => {
-            return (
-              <option key={pokemon.name} value={pokemon.name}>
-                {pokemon.name}
-              </option>
-            );
-          })}
-        </select>
-        {this.state.pokeName && <PokeCard pokemon={this.state.pokeName} />}
-      </div>
-    );
-  }
-}
-
-export default App;
-  */
-
-
-  /* import React, { useState, useEffect } from 'react'
-import "./styles.css";
-
-export default function App() {
-  const [password, setPassword] = useState('')
-  const [score, setScore] = useState(0)
-  const [message, setMessage] = useState('Senha fraca')
-
-  const handlePasswordInput = (event) => {
-    setPassword(event.target.value)
-  }
-
-  const handleMessage = () => {
-    if (score > 0 && score <= 3) {
-      setMessage('Senha fraca')
-    } else if (score >= 4 && score <= 6) {
-      setMessage('Senha média')
-    } else if (score > 6) {
-      setMessage('Senha forte')
-    }
+      })
   }
 
   useEffect(() => {
-    handleMessage()
+    handlePokemons()
   })
 
-  useEffect(() => {
-    let newScore = 0
-
-    if (password.length > 5) {
-      newScore = newScore + 1
-    }
-
-    if (password.length > 10) {
-      newScore += 2
-    }
-
-    if (/[A-Z]/.test(password)) {
-      newScore += 1
-    }
-
-    if (/\d/.test(password)) {
-      newScore += 1
-    }
-
-    if (
-      password.includes("!") ||
-      password.includes("@") ||
-      password.includes("#")
-    ) {
-      newScore += 2
-    }
-
-    setScore(newScore)
-  }, [password])
+  const changePokeName = event => {
+    setPokeName(event.target.value)
+  }
 
   return (
     <div className="App">
-      <h1>Força de senha</h1>
-      <input type="password" onChange={handlePasswordInput} value={password} />
-      <p>{message}</p>
-      <progress value={score} min={0} max={7} />
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map(pokemon => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+
+      {pokeName && <PokeCard pokemon={pokeName} />}
     </div>
   );
 }
- */
+
+export default App;
+
--- semana9/use-effect/pokeapiusee/src/Components/PokeCard.js
+++ semana9/use-effect/pokeapiusee/src/Components/PokeCard.js
@@ -1,46 +1,30 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-function PokeCard() {
-    const [pokemon, setPokemon] = useState()
+function PokeCard(props) {
+  const [pokemon, setPokemon] = useState({})
 
-   const pegaPokemon = (pokeName) => {
-        axios
-          .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-          .then(response => {
-            setPokemon(response.data)
-          })
-          .catch(err => {
-            console.log('Não foi possivel', err);
-          });
-      };
+  const pegaPokemon = (pokeName) => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .then((response) => {
+        setPokemon(response.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
-    //DID MOUNT
+  useEffect(() => {
+    pegaPokemon(props.pokemon);
+  })
 
- /*    useEffect(() => {
-        pegaPokemon(pokemon)
-      }) */
 
-     /*  useEffect((props) => {
-        pegaPokemon(props.pokemon)
-      }) */
-
-//DID UPDATE
-
-useEffect((prevProps) => {
-    console.log('Erro', prevProps)
-    if (prevProps.pokemon !== pokemon) {
-        pegaPokemon(pokemon.name);
-      }
-  }, [pokemon])
-
-/* useEffect((prevProps) => {
-    if (prevProps.pokemon !== props.pokemon) {
-        pegaPokemon(props.pokemon);
-      }
-  }, [pokemon]) */
-
-  /* const pokemon = this.state.pokemon; */
+  useEffect(() => {
+    if (pokemon !== props.pokemon) {
+      pegaPokemon(props.pokemon)
+    }
+  }, [props.pokemon])
 
   return (
     <div>
@@ -52,44 +36,7 @@ useEffect((prevProps) => {
       )}
     </div>
   );
-
-
-
-  /*
-  state = {
-    pokemon: {}
-  };
-  componentDidUpdate(prevProps) {
-    if (prevProps.pokemon !== this.props.pokemon) {
-      this.pegaPokemon(this.props.pokemon);
-    }
-  }
-
-  pegaPokemon = pokeName => {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-      .then(response => {
-        this.setState({ pokemon: response.data });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
-  
-    const pokemon = this.state.pokemon;
-
-    return (
-      <div>
-        <p>{pokemon.name}</p>
-        <p>{pokemon.weight} Kg</p>
-        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
-        {pokemon.sprites && (
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-        )}
-      </div>
-    ); */
-  
 }
 
 export default PokeCard;
+
