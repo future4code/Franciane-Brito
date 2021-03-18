a6a36b178c23940ee4d9b7d34a19ade62a1db070
franciane-brito <francianebrito99@gmail.com>
2021-03-17 23:11:07 -0300

    Atualizações

--- semana15/exercicio-quarta/src/index.ts
+++ semana15/exercicio-quarta/src/index.ts
@@ -8,15 +8,15 @@ app.use(express.json())
 app.use(cors())
 
 
-// app.get('/countries/all', (req: Request, res: Response) => {
-//     const myCountries = countries.map((country) => {
-//         return ({
-//             id: country.id,
-//             name: country.name
-//         })
-//     })
-//     res.status(201).send(myCountries);
-// });
+app.get('/countries/all', (req: Request, res: Response) => {
+    const myCountries = countries.map((country) => {
+        return ({
+            id: country.id,
+            name: country.name
+        })
+    })
+    res.status(201).send(myCountries);
+});
 
 
 app.get('/countries/:id', (req: Request, res: Response) => {
@@ -31,62 +31,56 @@ app.get('/countries/:id', (req: Request, res: Response) => {
     }
 })
 
-// app.get('/countries/search', (req: Request, res: Response) => {
-//     let result = countries
+app.get('/countries/search', (req: Request, res: Response) => {
+    let result = countries
 
-//     if (req.query.name) {
-//        result = result.filter(
-//           country => country.name.includes(req.query.name as string)
-//        )
-//     }
-    
-//     if (req.query.capital) {
-//        result = result.filter(
-//           country => country.capital.includes(req.query.capital as string)
-//        )
-//     }
-    
-//     if (req.query.continent) {
-//        result = result.filter(
-//           country => country.continent.includes(req.query.continent as string)
-//        )
-//     }
-//     if (result.length) {
-//         res.status(201).send({result});
-//     } else {
-//         res.status(404).send("Not Found");
-//     }
+    if (req.query.name) {
+        result = result.filter(
+            country => country.name.includes(req.query.name as string)
+        )
+    }
 
+    if (req.query.capital) {
+        result = result.filter(
+            country => country.capital.includes(req.query.capital as string)
+        )
+    }
 
-// });
+    if (req.query.continent) {
+        result = result.filter(
+            country => country.continent.includes(req.query.continent as string)
+        )
+    }
+    if (result.length) {
+        res.status(201).send({ result });
+    } else {
+        res.status(404).send("Not Found");
+    }
+});
 
 //4)
 app.put("/countries/edit/:id", (req: Request, res: Response) => {
     const myCountries = countries
     myCountries.forEach((country) => {
-        if(country.id === Number(req.params.id)){
+        if (country.id === Number(req.params.id)) {
             country.name = req.body.name
             country.capital = req.body.capital
             console.log(req.body.name)
-        } 
+        }
     })
-    res.status(201).send({myCountries});
-    
+    res.status(201).send({ myCountries });
+
 })
 
 // 5)
 app.delete('countries/:id', (req: Request, res: Response) => {
-    
 })
-
-
-
 
 const server = app.listen(process.env.PORT || 3018, () => {
     if (server) {
         const address = server.address() as AddressInfo;
         console.log(`Server is running in http://localhost:3003`)
-        
+
     } else {
         console.error(`Failure upon starting server.`)
     }
