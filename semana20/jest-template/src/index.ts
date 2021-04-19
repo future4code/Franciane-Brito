d18b916ca97634ace685a0f6a7c32dc0d0a9e5e5
franciane-brito <francianebrito99@gmail.com>
2021-04-19 16:51:45 -0300

    estruturando

---
+++ semana20/jest-template/src/index.ts
@@ -0,0 +1,19 @@
+import knex from 'knex'
+import { config } from 'dotenv'
+
+config()
+
+export const isEven = (integer: number): any => { }
+
+export const connection = knex({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        port: 3306,
+        multipleStatements: true
+
+    }
+});
